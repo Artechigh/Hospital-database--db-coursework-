bcfc95427a4cdd17dac810333fc12872fb41dbe3
StepanLebedevu <stepan_leb@mail.ru>
2023-05-15 01:40:25 +0300

    minor fix

--- src/components/404.js
+++ src/components/404.js
@@ -4,11 +4,7 @@ const NotFound = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
         <h1 className='text-4xl font-bold'>Извините, но страница, которую вы ищите - недоступна.</h1>
-<<<<<<< HEAD
         <div className='text-xl  p-5'>Пожалуйста, перейдите на главную страницу.</div>
-=======
-        <div>Пожалуйста, перейдите на главную страницу.</div>
->>>>>>> ccb4d705ebce62a8011aa4877294e61761e786fa
     </div>
   )
 }
--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -5,19 +5,11 @@ import Footer from './footer'
 const Wrapper= ({title, children}) => {
   return (
     <div className='bg-white text-black'>
-<<<<<<< HEAD
       <div>
           <title>{title ? title + " - MyHospital" : 'MyHospital DB'}</title>
           <meta name="description" content="Все данные о вашем здоровье - здесь."/>
           <link rel="icon" href="/favicon.ico"/>
         </div>
-=======
-      <head>
-          <title>{title ? title + " - MyHospital" : 'MyHospital DB'}</title>
-          <meta name="description" content="Все данные о вашем здоровье - здесь."/>
-          <link rel="icon" href="/favicon.ico"/>
-        </head>
->>>>>>> ccb4d705ebce62a8011aa4877294e61761e786fa
         <div className='flex min-h-screen min-w-screen flex-col justify-center items-center bg-slate-200'>
         <header>
             <Header />
