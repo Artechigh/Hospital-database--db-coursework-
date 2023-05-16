f4c6883db5f802c3867031b1ca9a6e52fc5a75ad
StepanLebedevu <stepan_leb@mail.ru>
2023-05-16 23:50:41 +0300

    Selector boxes fully functional (no API)

--- src/components/selector.jsx
+++ src/components/selector.jsx
@@ -1,79 +1,162 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react'
 import { BiChevronDown } from "react-icons/bi";
 import { AiOutlineSearch } from "react-icons/ai";
 
-const Selector = ({data}) => {
-  const [countries, setCountries] = useState(null);
+// cуну const сюда
+const hospitalsTemp = [{"name":"Больница им. Святого Александра И.","address":"ул. Большая Луговая, 17/29","phone":"+7(4852)24-31-31","doctors":[]},{"name":"H2","address":"street2","phone":"123123","doctors":[{"specialty":"surgeon","user":{"name":"me","birthDate":"2023-05-15T17:27:58.462Z","gender":""}},{"specialty":"ophthalmologists","user":{"name":"mem","birthDate":"1970-01-01T00:00:00.000Z","gender":""}}]}]
+//
+
+export default function Selector({getDoctorName}) {
+  const [hospitals, setHospitals] = useState(null);
+
   const [inputValue, setInputValue] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+
   const [selected, setSelected] = useState("");
+  const [selected2, setSelected2] = useState("");
+
   const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
 
   useEffect(() => {
-        test()
-        console.log(countries)
-  }, [data]);
+    // fetch("http://localhost:3000/data/hospitals")
+    fetch("https://restcountries.com/v2/all?fields=name")
+      .then((res) => res.json())
+      .then((data) => {
+        // setHospitals(data);
+        setHospitals(hospitalsTemp);
+      });
+  }, []);
 
-  async function test() {
-    setCountries(data);
-  }
 
-  return (
-    <div className="w-72 font-medium h-80">
-      <div
-        onClick={() => setOpen(!open)}
-        className={`bg-white w-full p-2 flex items-center justify-between rounded ${
-          !selected && "text-gray-700"
-        }`}
-      >
-        {selected
-          ? selected?.length > 25
-            ? selected?.substring(0, 25) + "..."
-            : selected
-          : "Select Country"}
-        <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
-      </div>
-      {/* <ul
-        className={`bg-white mt-2 overflow-y-auto ${
-          open ? "max-h-60" : "max-h-0"
-        } `}
-      >
-        <div className="flex items-center px-2 sticky top-0 bg-white">
-          <AiOutlineSearch size={18} className="text-gray-700" />
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value.toLowerCase())}
-            placeholder="Enter country name"
-            className="placeholder:text-gray-700 p-2 outline-none"
-          />
+  useEffect(() => {
+    setSelected2("")
+  }, [selected])
+
+  useEffect(() => {
+    // TODO забрать имя доктора
+  }, [selected2])
+    return (
+      <div className='flex flex-row'>
+        <div className="w-72 font-medium h-80 mr-10">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`bg-slate-300 w-full p-2 flex items-center justify-between rounded ${
+            !selected && "text-gray-700"
+          }`}
+        >
+          {selected
+            ? selected?.length > 25
+              ? selected?.substring(0, 25) + "..."
+              : selected
+            : "Выберите филиал:"}
+          <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
         </div>
-        {countries?.map((country) => (
-          <li
-            key={country?.name}
-            className={`p-2 text-sm hover:bg-sky-600 hover:text-white
-            ${
-              country?.name?.toLowerCase() === selected?.toLowerCase() &&
-              "bg-sky-600 text-white"
-            }
-            ${
-              country?.name?.toLowerCase().startsWith(inputValue)
-                ? "block"
-                : "hidden"
-            }`}
-            onClick={() => {
-              if (country?.name?.toLowerCase() !== selected.toLowerCase()) {
-                setSelected(country?.name);
-                setOpen(false);
-                setInputValue("");
+        <ul
+          className={`bg-slate-300 mt-2 overflow-y-auto ${
+            open ? "max-h-60" : "max-h-0"
+          } `}
+        >
+          <div className="flex items-center px-2 sticky top-0 bg-slate-400">
+            <AiOutlineSearch size={18} className="text-gray-700" />
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value.toLowerCase())}
+              placeholder="Поиск филиала"
+              className="placeholder:text-gray-700 bg-slate-400 p-2 outline-none"
+            />
+          </div>
+          {hospitals?.map((hospital) => (
+            <li
+              key={hospital?.name}
+              className={`p-2 text-sm hover:bg-sky-600 hover:text-white
+              ${
+                hospital?.name?.toLowerCase() === selected?.toLowerCase() &&
+                "bg-sky-600 text-white"
               }
-            }}
-          >
-            {country?.name}
-          </li>
-        ))}
-      </ul> */}
-    </div>
-  );
-};
+              ${
+                hospital?.name?.toLowerCase().startsWith(inputValue)
+                  ? "block"
+                  : "hidden"
+              }`}
+              onClick={() => {
+                if (hospital?.name?.toLowerCase() !== selected.toLowerCase()) {
+                  setSelected(hospital?.name);
+                  setOpen(false);
+                  setInputValue("");
+                }
+              }}
+            >
+              <div className='text-md pb-2 font-bold'>{hospital?.name}</div>
+              <div className='opacity-70'>{hospital?.phone}</div>
+              <div>{hospital?.address}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-export default Selector;
+
+
+
+      <div className="w-72 font-medium h-80">
+        <div
+          onClick={() => setOpen2(!open2)}
+          className={`bg-slate-300 w-full p-2 flex items-center justify-between rounded ${
+            !selected2 && "text-gray-700"
+          }`}
+        >
+          {selected2
+            ? selected2?.length > 25
+              ? selected2?.substring(0, 25) + "..."
+              : selected2
+            : "Выберите профиль:"}
+          <BiChevronDown size={20} className={`${open2 && "rotate-180"}`} />
+        </div>
+        <ul
+          className={`bg-slate-300 mt-2 overflow-y-auto ${
+            open2 ? "max-h-60" : "max-h-0"
+          } `}
+        >
+          <div className="flex items-center px-2 sticky top-0 bg-slate-400">
+            <AiOutlineSearch size={18} className="text-gray-700" />
+            <input
+              type="text"
+              value={inputValue2}
+              onChange={(e) => setInputValue2(e.target.value.toLowerCase())}
+              placeholder="Поиск профиля"
+              className="placeholder:text-gray-700 bg-slate-400 p-2 outline-none"
+            />
+          </div>
+          {hospitals?.filter(hosp => hosp?.name == selected)[0]?.doctors?.map((doctor) => (
+            <li
+              key={doctor?.specialty}
+              className={`p-2 text-sm hover:bg-sky-600 hover:text-white
+              ${
+                doctor?.specialty?.toLowerCase() === selected2?.toLowerCase() &&
+                "bg-sky-600 text-white"
+              }
+              ${
+                doctor?.specialty?.toLowerCase().startsWith(inputValue2)
+                  ? "block"
+                  : "hidden"
+              }
+              `}
+              onClick={() => {
+                if (doctor?.specialty?.toLowerCase() !== selected2?.toLowerCase()) {
+                  setSelected2(doctor?.specialty);
+                  setOpen2(false);
+                }
+              }}
+            >
+              {doctor?.specialty}
+            </li>
+          ))}
+          {!hospitals?.filter(hosp => hosp?.name == selected)[0]?.doctors[0] ? <li className='p-2 text-sm hover:bg-sky-600 hover:text-white'> Доктор не найден.</li> : <li></li>}
+        </ul>
+      </div>
+      DoctorName:
+      {hospitals?.filter(hosp => hosp?.name == selected)[0]?.doctors?.filter(doct => doct?.specialty == selected2)[0]?.user?.name}
+    </div>
+    );
+  }
--- src/routes/appointment.jsx
+++ src/routes/appointment.jsx
@@ -1,133 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { useId } from 'react';
-import { BiChevronDown } from "react-icons/bi";
-import { AiOutlineSearch } from "react-icons/ai";
+import Selector from '../components/selector.jsx';
 
-// cуну const сюда
-const hospitalsTemp = [{"name":"H1","address":"street1","phone":"890890","doctors":[]},{"name":"H2","address":"street2","phone":"123123","doctors":[{"specialty":"surgeon","user":{"name":"me","birthDate":"2023-05-15T17:27:58.462Z","gender":""}},{"specialty":"ophthalmologists","user":{"name":"mem","birthDate":"1970-01-01T00:00:00.000Z","gender":""}}]}]
-//
 
 export default function Appointment() {
-  const [hospitals, setHospitals] = useState(null);
-  const [inputValue, setInputValue] = useState("");
-  const [selected, setSelected] = useState("");
-  const [selected2, setSelected2] = useState("");
-  const [open, setOpen] = useState(false);
-  const [open2, setOpen2] = useState(false);
-
-  useEffect(() => {
-    fetch("https://restcountries.com/v2/all?fields=name")
-      .then((res) => res.json())
-      .then((data) => {
-        setHospitals(hospitalsTemp);
-      });
-  }, []);
-
-
-  useEffect(() => {
-
-  }, [])
+  
     return (
-      <div className='flex flex-row'>
-        <div className="w-72 font-medium h-80">
-        <div
-          onClick={() => setOpen(!open)}
-          className={`bg-slate-300 w-full p-2 flex items-center justify-between rounded ${
-            !selected && "text-gray-700"
-          }`}
-        >
-          {selected
-            ? selected?.length > 25
-              ? selected?.substring(0, 25) + "..."
-              : selected
-            : "Выберите профиль:"}
-          <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
-        </div>
-        <ul
-          className={`bg-slate-300 mt-2 overflow-y-auto ${
-            open ? "max-h-60" : "max-h-0"
-          } `}
-        >
-          <div className="flex items-center px-2 sticky top-0 bg-white">
-            <AiOutlineSearch size={18} className="text-gray-700" />
-            <input
-              type="text"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value.toLowerCase())}
-              placeholder="Поиск профиля"
-              className="placeholder:text-gray-700 p-2 outline-none"
-            />
-          </div>
-          {hospitals?.map((hospital) => (
-            <li
-              key={hospital?.name}
-              className={`p-2 text-sm hover:bg-sky-600 hover:text-white
-              ${
-                hospital?.name?.toLowerCase() === selected2?.toLowerCase() &&
-                "bg-sky-600 text-white"
-              }
-              ${
-                hospital?.name?.toLowerCase().startsWith(inputValue)
-                  ? "block"
-                  : "hidden"
-              }`}
-              onClick={() => {
-                if (hospital?.name?.toLowerCase() !== selected.toLowerCase()) {
-                  setSelected(hospital?.name);
-                  setOpen(false);
-                  setInputValue("");
-                }
-              }}
-            >
-              {hospital?.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-
-
-
-      <div className="w-72 font-medium h-80">
-        <div
-          onClick={() => setOpen2(!open2)}
-          className={`bg-slate-300 w-full p-2 flex items-center justify-between rounded ${
-            !selected2 && "text-gray-700"
-          }`}
-        >
-          {selected2
-            ? selected2?.length > 25
-              ? selected2?.substring(0, 25) + "..."
-              : selected2
-            : "Выберите филиал:"}
-          <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
-        </div>
-        <ul
-          className={`bg-slate-300 mt-2 overflow-y-auto ${
-            open2 ? "max-h-60" : "max-h-0"
-          } `}
-        >
-          {hospitals?.map((country) => (
-            <li
-              key={country?.name}
-              className={`p-2 text-sm hover:bg-sky-600 hover:text-white
-              ${
-                country?.name?.toLowerCase() === selected?.toLowerCase() &&
-                "bg-sky-600 text-white"
-              }
-              `}
-              onClick={() => {
-                if (country?.name?.toLowerCase() !== selected2.toLowerCase()) {
-                  setSelected2(country?.name);
-                  setOpen2(false);
-                }
-              }}
-            >
-              {country?.name}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-xl py-10 font-bold'>Выберите необходимый филиал и профиль специалиста для записи</h1>
+        <Selector />
+        {/* TODO забрать имя доктора и отправить на фетч форме записи к доктору */}
     </div>
     );
   }
