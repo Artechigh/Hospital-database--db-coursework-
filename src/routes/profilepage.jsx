15284e49918fb6c8a2e50a7809f786ce742869df
StepanLebedevu <stepan_leb@mail.ru>
2023-05-19 17:11:16 +0300

    jjkuygjhvb

--- src/routes/profilepage.jsx
+++ src/routes/profilepage.jsx
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react'
 
 const ProfilePage = () => {
     const user = sessionStorage.getItem('user')
-    const [isDoctor, setIsDoctor] = useState(null)
+    const [isDoctor, setIsDoctor] = useState("")
 
     useEffect(() => {
       if (JSON.parse(user)?.Patient) {
-        setIsDoctor(false)
+        setIsDoctor("patient")
       } else if (JSON.parse(user)?.Doctor) {
-        setIsDoctor(true)
+        setIsDoctor("doctor")
       }
     }, [])
     
@@ -17,9 +17,7 @@ const ProfilePage = () => {
   return (
     <div>
         {JSON.parse(user)?.name}
-        {
-            isDoctor ? <div> ВЫ - ДОКТОР =3 </div> : <div> ВЫ - пациент </div>
-        }
+        {isDoctor}
     </div>
   )
 }
