43e0f9b670fac19e37e202bd08931dbe05d26172
StepanLebedevu <stepan_leb@mail.ru>
2023-05-19 15:12:38 +0300

    1

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -116,7 +116,7 @@ const MyCalendar = ({dId, hId}) => {
         date: `2023-05-${startDay}T${startHour}:00:00.000Z`,
         doctorId: +dId,
         hospitalId: +hId,
-        patientId: 666
+        patientId: 1
       }).then((response) => {
         console.log(response.data);
       })
