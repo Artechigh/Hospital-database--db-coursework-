47c2499ab37b0c8e067763e32ad62be1686e83bd
StepanLebedevu <stepan_leb@mail.ru>
2023-05-19 15:30:11 +0300

    fff

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -11,9 +11,6 @@ const Login = () => {
 
   const handleSubmit = (e) => {
       e.preventDefault();
-
-
-
        axios.post("http://localhost:3000/api/login", {
         email: e.target.email.value,
         password: e.target.password.value,
@@ -28,6 +25,7 @@ const Login = () => {
 
   return (
     <div className='flex flex-col justify-center items-center'>
+      <div className='font-semibold text-2xl pb-11'>Введите данные для входа</div>
       <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center space-y-5 '>
                 <input type='text' name='email' placeholder='Почта'
                   className='bg-slate-200 text-slate-700 border-b-2 border-slate-700 p-2 w-96'
