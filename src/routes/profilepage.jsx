05558593040663aaca9a7db2aa539fc93376e1a2
StepanLebedevu <stepan_leb@mail.ru>
2023-05-19 19:55:09 +0300

    no-sotr no-post

--- src/routes/profilepage.jsx
+++ src/routes/profilepage.jsx
@@ -1,31 +1,35 @@
 import React, { useEffect, useState } from 'react'
 
 const tempApp = {"Appointments":[{"id":1,"date":"2023-05-18T10:00:00.000Z","diagnosis":"","hospitalId":1,"doctorId":1,"patientId":1},{"id":5,"date":"2023-05-15T10:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":1},{"id":7,"date":"2023-05-16T10:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":1}]}
-const tempHospApp = {"Hospitals":[{"name":"Первая городская больница"}],"Appointments":[{"id":1,"date":"2023-05-18T10:00:00.000Z","diagnosis":"","hospitalId":1,"doctorId":1,"patientId":1},{"id":2,"date":"2023-05-18T11:00:00.000Z","diagnosis":"","hospitalId":1,"doctorId":1,"patientId":2},{"id":5,"date":"2023-05-15T10:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":1},{"id":6,"date":"2023-05-16T11:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":2},{"id":7,"date":"2023-05-16T10:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":1}]}
+const tempHospApp = {"Hospitals":[{"name":"Первая городская больница"}],
+"Appointments":[{"id":1,"date":"2023-05-18T10:00:00.000Z","diagnosis":"","hospitalName":"name","doctorId":1,"patientName":"name"},{"id":2,"date":"2023-05-18T11:00:00.000Z","diagnosis":"","hospitalId":1,"doctorId":1,"patientId":2},{"id":5,"date":"2023-05-15T10:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":1},{"id":6,"date":"2023-05-16T11:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":2},{"id":7,"date":"2023-05-16T10:00:00.000Z","diagnosis":null,"hospitalId":1,"doctorId":1,"patientId":1}]}
 
 
 const ProfilePage = () => {
     const user = sessionStorage.getItem('user')
     const [isDoctor, setIsDoctor] = useState("")
+    const [info, setInfo] = useState(null)
 
     useEffect(() => {
-      if (JSON.parse(user)?.Patient) {
-        setIsDoctor("patient")
-        axios.post("http://localhost:3000/data/patient/appointments", {
-          id: JSON.parse(user)?.Patient?.id,
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-      } else if (JSON.parse(user)?.Doctor) {
-        setIsDoctor("doctor")
-        axios.post("http://localhost:3000/data/doctor/hopitalsAndAppointments", {
-          id: JSON.parse(user)?.Doctor?.id,
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-      }
+      // if (JSON.parse(user)?.Patient) {
+      //   setIsDoctor("patient")
+      //   axios.post("http://localhost:3000/data/patient/appointments", {
+      //     id: JSON.parse(user)?.Patient?.id,
+      //   })
+      //   .then((response) => {
+      //     setInfo(response.data);
+      //   })
+      // } else if (JSON.parse(user)?.Doctor) {
+      //   setIsDoctor("doctor")
+      //   axios.post("http://localhost:3000/data/doctor/hopitalsAndAppointments", {
+      //     id: JSON.parse(user)?.Doctor?.id,
+      //   })
+      //   .then((response) => {
+      //     setInfo(response.data);
+      //   })
+      // }
+      setInfo(tempHospApp)
+      setIsDoctor("doctor")
     }, [])
     
     
@@ -33,25 +37,47 @@ const ProfilePage = () => {
   return (
     <div>
         <div className='flex flex-col justify-center items-start space-y-4'>
-            <div className='font-bold text-4xl pb-3'>
+            <div className='font-bold text-2xl pb-3'>
                 {JSON.parse(user)?.name}
             </div>
-            <div className='text-xl'>
+            <div className='text-lg'>
                 Электронная почта: {JSON.parse(user)?.email}
             </div>
-            <div className='text-xl'>
+            <div className='text-lg'>
                 Дата рождения: {JSON.parse(user)?.birthDate}
             </div>
-            <div className='text-xl'>
+            <div className='text-lg'>
                 Пол: {JSON.parse(user)?.gender.substring(0, 10)}
             </div>
         </div>
-        {isDoctor
-            // больницы, аппоинтменты
+        {isDoctor === "doctor" ?
+            <div>
+              <div className='text-lg font-medium pt-8 pb-2'>Больницы, в которых вы работаете:</div>
+              <div className='flex flex-col justify-start'>
+                {info?.Hospitals?.map(hospital => (
+                  <div className='font-light'>
+                    {hospital?.name}
+                  </div>
+                ))}
+              </div>
+
+              <div className='text-lg font-medium pt-8 pb-2'>Список ближайших записей:</div>
+              <div className='flex flex-col justify-start'>
+                  {
+                    info?.Appointments?.map(appointment => (
+                      <div className='py-3 px-5 border-slate-800 border-2 rounded-md'>
+                        <div className='font-medium pb-2'>{appointment?.patientName}</div>
+                        <div className='font-light'>{appointment?.date.substring(0,10)} в {appointment?.date.substring(11,16)}</div>
+                        <div className='font-light text-sm'>{appointment?.hospitalName}</div>
+                      </div>
+                    ))
+                  }
+              </div>
+            </div> : <></>
         }
-        {/* {!isDoctor
+        {isDoctor === "patient"
             // Аппоинтмнты, прескрипшн
-        } */}
+        }
     </div>
   )
 }
